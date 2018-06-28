025cb11e60703ce46402c3697cacc9888f66304d
Ueli Kunz <kunz@ideadapt.net>
2018-06-29 01:09:38 +0200

    only do work if in test file. use AbstractWalker to control node traversal, this enables us to dive into nested callback stacks.

--- src/expectLengthRule.ts
+++ src/expectLengthRule.ts
@@ -3,32 +3,55 @@ import * as tsutils from "tsutils";
 import * as ts from "typescript";
 
 export class Rule extends Lint.Rules.AbstractRule {
-  public static FAILURE_STRING = "Length check for %s required";
-  public static ITERATOR_METHOD = ["forEach"];
+  public static FAILURE_STRING = "Length check required (e.g. 'expect(xy.length).toBeGreaterThan(0);' )";
+  public static ITERATOR_METHODS = ["forEach"];
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new ExpectLengthWalker(sourceFile, this.getOptions()));
+    return this.applyWithWalker(
+      new ExpectLengthWalker(sourceFile, this.ruleName, new Set(this.ruleArguments.map(String))));
   }
 }
 
 // tslint:disable:max-line-length
 // tslint:disable-next-line:max-classes-per-file
-class ExpectLengthWalker extends Lint.RuleWalker {
+class ExpectLengthWalker extends Lint.AbstractWalker<Set<string>> {
   private usages: Map<ts.Identifier, tsutils.VariableInfo>;
+  private isInTestFile = false;
 
-  constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
-    super(sourceFile, options);
-    this.usages = tsutils.collectVariableUsage(this.getSourceFile());
+  constructor(sourceFile: ts.SourceFile, ruleName: string, options: Set<string>) {
+    super(sourceFile, ruleName, options);
+    this.isInTestFile = sourceFile.fileName.includes("/test/")
+                        || sourceFile.fileName.includes("/e2e/")
+                        || sourceFile.fileName.endsWith("spec.ts");
+
+    if (this.isInTestFile) {
+      this.usages = tsutils.collectVariableUsage(this.getSourceFile());
+    }
   }
 
+  public walk(sourceFile: ts.SourceFile) {
+    const cb = (node: ts.Node): void => {
+        if (node.kind === ts.SyntaxKind.CallExpression) {
+            this.visitCallExpression(node as ts.CallExpression);
+            return ts.forEachChild(node, cb);
+        } else {
+            // Continue rescursion: call function `cb` for all children of the current node.
+            return ts.forEachChild(node, cb);
+        }
+    };
+    return ts.forEachChild(sourceFile, cb);
+}
+
   public visitCallExpression(node: ts.CallExpression) {
+    if (!this.isInTestFile) { return; }
+
     const memberExpressionCalled = node.expression.getChildCount() === 3
                                     && node.expression.getChildAt(0).kind === ts.SyntaxKind.Identifier;
     if (memberExpressionCalled) {
         const callee: ts.Identifier = node.expression.getChildAt(0) as ts.Identifier;
         const methodName: string = node.expression.getChildAt(2).getText();
 
-        if (Rule.ITERATOR_METHOD.indexOf(methodName) >= 0 && !this.isLengthExpectedFor(callee)) {
+        if (Rule.ITERATOR_METHODS.indexOf(methodName) >= 0 && !this.isLengthExpectedFor(callee)) {
           this.addFailureAt(callee.getStart(), callee.getText().length, Rule.FAILURE_STRING);
         }
     }
