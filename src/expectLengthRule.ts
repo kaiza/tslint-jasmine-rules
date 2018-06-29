32cdf4aa3e4a3076024b7c87946863fe1fdef3dc
Ueli Kunz <kunz@ideadapt.net>
2018-06-29 09:28:02 +0200

    some optimizations and refactorings

--- src/expectLengthRule.ts
+++ src/expectLengthRule.ts
@@ -3,26 +3,32 @@ import * as tsutils from "tsutils";
 import * as ts from "typescript";
 
 export class Rule extends Lint.Rules.AbstractRule {
-  public static FAILURE_STRING = "Length check required (e.g. 'expect(xy.length).toBeGreaterThan(0);' )";
+  public static FAILURE_STRING =
+    "Length check required (e.g. 'expect(xy.length).toBeGreaterThan(0);' )";
   public static ITERATOR_METHODS = ["forEach"];
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     return this.applyWithWalker(
-      new ExpectLengthWalker(sourceFile, this.ruleName, new Set(this.ruleArguments.map(String))));
+      new ExpectLengthWalker(sourceFile, this.ruleName, new Set(this.ruleArguments.map(String))),
+    );
   }
 }
 
-// tslint:disable:max-line-length
 // tslint:disable-next-line:max-classes-per-file
 class ExpectLengthWalker extends Lint.AbstractWalker<Set<string>> {
   private usages: Map<ts.Identifier, tsutils.VariableInfo>;
   private isInTestFile = false;
+  private minimalPattern = "(\/test\/)";
+  private defaultPattern = "(\/e2e\/)|(spec\.ts$)";
+  private fileNamePattern = new RegExp(this.minimalPattern + "|" + this.defaultPattern);
 
-  constructor(sourceFile: ts.SourceFile, ruleName: string, options: Set<string>) {
+  constructor(
+    sourceFile: ts.SourceFile,
+    ruleName: string,
+    options: Set<string>,
+  ) {
     super(sourceFile, ruleName, options);
-    this.isInTestFile = sourceFile.fileName.includes("/test/")
-                        || sourceFile.fileName.includes("/e2e/")
-                        || sourceFile.fileName.endsWith("spec.ts");
+    this.isInTestFile = this.fileNamePattern.test(sourceFile.fileName);
 
     if (this.isInTestFile) {
       this.usages = tsutils.collectVariableUsage(this.getSourceFile());
@@ -31,39 +37,41 @@ class ExpectLengthWalker extends Lint.AbstractWalker<Set<string>> {
 
   public walk(sourceFile: ts.SourceFile) {
     const cb = (node: ts.Node): void => {
-        if (node.kind === ts.SyntaxKind.CallExpression) {
-            this.visitCallExpression(node as ts.CallExpression);
-            return ts.forEachChild(node, cb);
-        } else {
-            // Continue rescursion: call function `cb` for all children of the current node.
-            return ts.forEachChild(node, cb);
-        }
+      if (node.kind === ts.SyntaxKind.CallExpression) {
+        this.visitCallExpression(node as ts.CallExpression);
+      }
+      return ts.forEachChild(node, cb);
     };
     return ts.forEachChild(sourceFile, cb);
-}
+  }
 
   public visitCallExpression(node: ts.CallExpression) {
-    if (!this.isInTestFile) { return; }
+    if (!this.isInTestFile) {
+      return;
+    }
 
-    const memberExpressionCalled = node.expression.getChildCount() === 3
-                                    && node.expression.getChildAt(0).kind === ts.SyntaxKind.Identifier;
+    const memberExpressionCalled =
+      node.expression.getChildCount() === 3 &&
+      node.expression.getChildAt(0).kind === ts.SyntaxKind.Identifier;
+
     if (memberExpressionCalled) {
-        const callee: ts.Identifier = node.expression.getChildAt(0) as ts.Identifier;
-        const methodName: string = node.expression.getChildAt(2).getText();
+      const callee = node.expression.getChildAt(0) as ts.Identifier;
+      const methodName = node.expression.getChildAt(2).getText();
+      const iteratingMethod = Rule.ITERATOR_METHODS.indexOf(methodName) >= 0;
 
-        if (Rule.ITERATOR_METHODS.indexOf(methodName) >= 0 && !this.isLengthExpectedFor(callee)) {
-          this.addFailureAt(callee.getStart(), callee.getText().length, Rule.FAILURE_STRING);
-        }
+      if (iteratingMethod && !this.isLengthExpectedFor(callee)) {
+        this.addFailureAt(callee.getStart(), callee.getText().length, Rule.FAILURE_STRING);
+      }
     }
   }
 
   private isLengthExpectedFor(callee: ts.Identifier) {
     let found = false;
     this.usages.forEach((value) => {
-      value.uses.forEach((use) => {
+      found = value.uses.some((use) => {
         const usage = use.location.parent.parent.getText();
         if (usage === "expect(" + callee.getText() + ".length)") {
-          found = true;
+          return true;
         }
       });
     });
