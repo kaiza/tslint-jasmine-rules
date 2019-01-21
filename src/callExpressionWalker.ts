dda706e89b1605a5e5fde1c77d6dad74207c93f3
aleclarson <alec.stanford.larson@gmail.com>
2019-01-20 19:55:29 -0500

    feat: skip auto-fix for warnings

--- src/callExpressionWalker.ts
+++ src/callExpressionWalker.ts
@@ -1,9 +1,15 @@
 import * as Lint from "tslint";
-import {IOptions} from "tslint";
+import {IOptions, RuleSeverity} from "tslint";
 import * as ts from "typescript";
 
 export class CallExpressionWalker extends Lint.RuleWalker {
-  constructor(sourceFile: ts.SourceFile, options: IOptions, private regex: RegExp, private failureString: string) {
+  constructor(
+    sourceFile: ts.SourceFile,
+    options: IOptions,
+    private regex: RegExp,
+    private failureString: string,
+    private ruleSeverity: RuleSeverity,
+  ) {
     super(sourceFile, options);
   }
 
@@ -11,7 +17,10 @@ export class CallExpressionWalker extends Lint.RuleWalker {
     const match = node.expression.getText().match(this.regex);
 
     if (match) {
-      const fix = Lint.Replacement.deleteText(node.getStart(), 1);
+      const fix = this.ruleSeverity === "error"
+        ? Lint.Replacement.deleteText(node.getStart(), 1)
+        : undefined;
+
       this.addFailureAt(node.getStart(), match[0].length, this.failureString, fix);
     }
 
